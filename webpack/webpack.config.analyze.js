70bb0a3ab356e0720bff2d518f3c7ebd28237750
WebsiteAB <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-04-30 14:00:17 +0200

    Add bundle analyze

---
+++ webpack/webpack.config.analyze.js
@@ -0,0 +1,10 @@
+const prod = require('./webpack.config.prod');
+const {merge} = require('webpack-merge');
+const BundleAnalyzerPlugin =
+	require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(prod, {
+	plugins: [
+		new BundleAnalyzerPlugin({analyzerMode: 'server', openAnalyzer: true}),
+	],
+});
